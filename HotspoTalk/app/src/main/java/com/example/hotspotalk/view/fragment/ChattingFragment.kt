fbe5325cfe026617d9226808d150ac382770058a
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-09-29 17:55:30 +0900

    [ADD]네비게이션

--- HotspoTalk/app/src/main/java/com/example/hotspotalk/view/fragment/ChattingFragment.kt
+++ HotspoTalk/app/src/main/java/com/example/hotspotalk/view/fragment/ChattingFragment.kt
@@ -21,7 +21,7 @@ class ChattingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.chatting_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_chatting, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
