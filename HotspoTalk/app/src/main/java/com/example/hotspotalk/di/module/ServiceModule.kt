facfb6dafe4ef3f9afeded28ac2a3adffa7b793a
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-10-27 16:59:15 +0900

    Merge branch 'main' of /Users/jinstonlee/Documents/HotspoTalk with conflicts.

--- HotspoTalk/app/src/main/java/com/example/hotspotalk/di/module/ServiceModule.kt
+++ HotspoTalk/app/src/main/java/com/example/hotspotalk/di/module/ServiceModule.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
+
     @Singleton
     @Provides
     fun provideAccountService(retrofit: Retrofit): AccountService =
