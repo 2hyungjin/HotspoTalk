2dbb4fe1e343e60b63bf7300118636caf97232b5
tmddusqls <tmddusqls@naver.com>
2021-11-03 19:43:25 +0900

    login

--- HotspoTalk/app/src/main/java/com/example/hotspotalk/view/fragment/LoginFragment.kt
+++ HotspoTalk/app/src/main/java/com/example/hotspotalk/view/fragment/LoginFragment.kt
@@ -1,12 +1,18 @@
 package com.example.hotspotalk.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.example.hotspotalk.R
+import com.example.hotspotalk.databinding.FragmentLoginBinding
+import com.example.hotspotalk.view.util.Preference.token
 import com.example.hotspotalk.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,15 +23,40 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
-    private val viewModel: LoginViewModel by activityViewModels()
+    private lateinit var binding: FragmentLoginBinding
 
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observe()
+    }
+
+    private fun observe() = with(viewModel) {
+        isSuccess.observe(viewLifecycleOwner) {
+            // todo 토큰 저장
+            when (it) {
+                null ->
+                    Toast.makeText(requireContext(), "아이디 또는 비밀번호가 옳지 않습니다.", Toast.LENGTH_SHORT).show()
+                else ->
+                    token = it.token!!
+            }
+        }
+
+        isFailure.observe(viewLifecycleOwner) {
+            Log.d("login", it)
+            Toast.makeText(requireContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
--- HotspoTalk/app/src/main/java/com/example/hotspotalk/viewmodel/LoginViewModel.kt
+++ HotspoTalk/app/src/main/java/com/example/hotspotalk/viewmodel/LoginViewModel.kt
@@ -5,13 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hotspotalk.data.entity.repuest.Login
 import com.example.hotspotalk.data.entity.response.Token
+import com.example.hotspotalk.data.repository.AccountRepository
+import com.example.hotspotalk.data.repository.RoomsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(): ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val accountRepository: AccountRepository
+): ViewModel() {
 
     val id = MutableLiveData("")
     val pw = MutableLiveData("")
@@ -22,15 +26,19 @@ class LoginViewModel @Inject constructor(): ViewModel() {
     private val _isFailure = MutableLiveData<String>()
     val isFailure = _isFailure
 
-    // todo
-    private val devToken = ""
+    val devToken = ""
 
     fun login() {
         viewModelScope.launch {
             val login = Login(id.value!!, pw.value!!, devToken)
 
+            val loginResponse = accountRepository.postLogin(login)
             when {
+                loginResponse.isSuccessful ->
+                    _isSuccess.value = loginResponse.body()
 
+                loginResponse.code() in 400..499 ->
+                    _isFailure.value = loginResponse.message()
             }
         }
     }
