67186992deac78e85d7e3c68f70ee266af0be764
양윤재 <diddbswo04@dgsw.hs.kr>
2021-11-05 21:58:13 +0900

    [ADD] Firebase

--- HotspoTalk/app/src/main/java/com/example/hotspotalk/context/HotspoTalkFirebaseMessagingService.kt
+++ HotspoTalk/app/src/main/java/com/example/hotspotalk/context/HotspoTalkFirebaseMessagingService.kt
@@ -1,15 +1,12 @@
 package com.example.hotspotalk.context
 
-import android.R
+import com.example.hotspotalk.R
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.example.hotspotalk.view.activity.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -23,6 +20,7 @@ class HotspoTalkFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent =
@@ -33,7 +31,6 @@ class HotspoTalkFirebaseMessagingService : FirebaseMessagingService() {
             NotificationManager.IMPORTANCE_HIGH
         ).apply {
             this.setShowBadge(true)
-
         }
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -41,15 +38,14 @@ class HotspoTalkFirebaseMessagingService : FirebaseMessagingService() {
 
 
         message.notification?.let {
-            Log.d("FCM", "body : ${it.body}")
 
             val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setContentTitle(it.title)
                 .setContentText(it.body)
-                .setSmallIcon(R.drawable.sym_def_app_icon)
+                .setSmallIcon(R.drawable.ic_logo)
                 .setFullScreenIntent(pendingIntent, true)
                 .setDefaults(Notification.DEFAULT_ALL)
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-            notificationBuilder.priority = NotificationCompat.PRIORITY_HIGH
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
 
             notificationManager.notify(1, notificationBuilder.build())
         }
