a7ec5fdd59859a78a61c0fc1e016d40aea8870e1
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-11-06 19:33:18 +0900

    [ADD] xml

--- HotspoTalk/app/src/main/java/com/example/hotspotalk/view/fragment/EnteredRoomFragment.kt
+++ HotspoTalk/app/src/main/java/com/example/hotspotalk/view/fragment/EnteredRoomFragment.kt
@@ -34,7 +34,7 @@ class EnteredRoomFragment : Fragment(),
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_home_vp_item_entered,
